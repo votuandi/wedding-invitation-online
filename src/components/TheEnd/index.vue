4b2caf2a2e3ef3a12a1005a4d4c39ebf9260b5ab
divt.it97 <41782079+votuandi@users.noreply.github.com>
2023-03-21 10:50:40 +0700

    update content

--- src/components/TheEnd/index.vue
+++ src/components/TheEnd/index.vue
@@ -1,24 +1,6 @@
 <template>
   <div class="w-full flex flex-col items-center">
-    <div
-      class="img-children relative justify-center h-fit py-2 w-screen md:w-[600px] items-center pb-5 self-center flex flex-col"
-    >
-      <div class="self-start p-6 animate__animated animate__pulse">
-        <p class="font-bold text-3xl text-rose-300">Bật mí chuyện "Xin vía"</p>
-        <p class="text-base font-light mt-1">
-          Tháng 5/2021,<br />
-          Chúng mình có tham dự tiệc cưới của một người bạn. Và...<br />
-          Nàng đã bắt được hoa cưới và trao cho chàng ❤️
-        </p>
-      </div>
-      <video
-        ref="video_vx_ref"
-        id="video-vx"
-        controls="true"
-        class="w-[320px] md:w-[640px] lg:w-[720px] h-auto self-center animate__animated animate__fadeInDown"
-        src="@/assets/videos/bat-hoa.mp4"
-      />
-    </div>
+    
     <div
       class="w-[100vw] h-[100vh] bg-white justify-center items-center flex flex-col"
     >
